8ddb02a531ef7dfc69464305f4b9025d31f75380
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2024-07-28 23:26:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 const busRoutes = require('./routes/busRoutes');
+const cors = require('cors');//edit
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;//edit
 require('dotenv').config();
-
+app.use(cors());//edit
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -22,7 +23,11 @@ db.once('open', () => {
     console.log('Connected to MongoDB Atlas');
 });
 
-app.use('/', busRoutes);
+app.use('/api', busRoutes);//edit
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});//new add
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
